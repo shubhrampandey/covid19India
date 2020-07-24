8832f008c95605d50ac213465db47677b0a6272b
shubhrampandey <45200005+shubhrampandey@users.noreply.github.com>
2020-07-24 18:59:30 +0530

    Update data
    1. Update data
    2. Added some checks to the functions

--- R/updateStateWise.R
+++ R/updateStateWise.R
@@ -3,23 +3,28 @@
 #' @param branch default branch is master
 #' @examples updateStateWise()
 #' @author Shubhram Pandey
-#' @export updateStateWise
+#' @export
 updateStateWise <- function(branch = "master"){
-
   `%>%` <- magrittr::`%>%`
   print(paste0("Branch name:",branch))
-
   if (branch != "master") {
     stop("The branch argument is missing")
   }
+  stateWiseOld = covid19India::stateWise
   stateWise <- read.csv("https://api.covid19india.org/csv/latest/state_wise.csv",
                           stringsAsFactors = FALSE) %>%
     dplyr::select(-Migrated_Other)
     if (ncol(stateWise) != 11) {
       stop("The number of columns is invalid")
     }
+  if (stateWiseOld$Confirmed[1] != stateWiseOld$Confirmed[1]) {
+    print("Updates available")
     usethis::use_data(stateWise, overwrite = TRUE)
+    save(stateWise, file = "data/stateWise.Rda")
     write.csv(stateWise, "csv/stateWise.csv", row.names = FALSE)
     print("The stateWise dataset was updated")
-    return(print("Done..."))
+  } else {
+    print("Updates are not available")
+  }
+  return(print("Done..."))
 }
--- R/updateTimeSeries.R
+++ R/updateTimeSeries.R
@@ -5,20 +5,29 @@
 #' @author Shubhram Pandey
 #' @export
 updateTimeSeriesIndia <- function(branch = "master"){
-
   `%>%` <- magrittr::`%>%`
   print(paste0("Branch name:",branch))
 
   if (branch != "master") {
     stop("The branch argument is missing")
   }
+  timeSeriesIndiaOld = covid19India::timeSeriesIndia
   timeSeriesIndia <- read.csv("https://api.covid19india.org/csv/latest/case_time_series.csv",
-                         stringsAsFactors = FALSE)
+                         stringsAsFactors = FALSE) %>%
+                         select(-Date) %>%
+                         mutate(Date = seq(as.Date("2020-01-30"),as.Date("2020-01-30") + (nrow(.) - 1),by = 1)) %>%
+    select(Date,dplyr::everything())
   if (ncol(timeSeriesIndia) != 7) {
     stop("The number of columns is invalid")
   }
-  usethis::use_data(timeSeriesIndia, overwrite = TRUE)
-  write.csv(timeSeriesIndia, "csv/timeSeriesIndia.csv", row.names = FALSE)
-  print("The time series dataset was updated")
+  if (nrow(timeSeriesIndia) > nrow(timeSeriesIndiaOld)) {
+    print("Updates available")
+    usethis::use_data(timeSeriesIndia, overwrite = TRUE)
+    save(timeSeriesIndia, file = "data/timeSeriesIndia.Rda")
+    write.csv(timeSeriesIndia, "csv/timeSeriesIndia.csv", row.names = FALSE)
+    print("The time series dataset was updated")
+  } else {
+    print("Updates are not available")
+  }
   return(print("Done..."))
 }
